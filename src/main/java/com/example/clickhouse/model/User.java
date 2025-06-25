0b08a4b61f10c346d8f6975ac1e7e14586d3d3a5
khamdam <khamdamsaparov@gmail.com>
2025-06-25 10:14:58 +0500

    Click house with Spring boot(Optimized)

--- src/main/java/com/example/clickhouse/model/User.java
+++ src/main/java/com/example/clickhouse/model/User.java
@@ -1,28 +1,22 @@
 package com.example.clickhouse.model;
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
   private Long id;
   private String name;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public User(Long id, String name) {
-    this.id = id;
-    this.name = name;
-  }
+  private String address;
+  private String phone;
+  private String email;
+  private String password;
+  private String pin;
+  private String gender;
+  private LocalDate birthday;
 }
--- src/main/java/com/example/clickhouse/model/UserRepository.java
+++ src/main/java/com/example/clickhouse/model/UserRepository.java
@@ -14,11 +14,35 @@ public class UserRepository {
   }
 
   public List<User> findAll() {
-    return jdbc.query("SELECT id, name FROM user", (rs, rowNum) ->
-        new User(rs.getLong("id"), rs.getString("name")));
+    return jdbc.query(
+        "SELECT id, name, address, phone, email, password, pin, gender, birthday FROM user",
+        (rs, rowNum) -> new User(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getString("address"),
+            rs.getString("phone"),
+            rs.getString("email"),
+            rs.getString("password"),
+            rs.getString("pin"),
+            rs.getString("gender"),
+            rs.getDate("birthday").toLocalDate()
+        )
+    );
   }
 
   public void save(User user) {
-    jdbc.update("INSERT INTO user (id, name) VALUES (?, ?)", user.getId(), user.getName());
+    jdbc.update(
+        "INSERT INTO user (id, name, address, phone, email, password, pin, gender, birthday) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        user.getId(),
+        user.getName(),
+        user.getAddress(),
+        user.getPhone(),
+        user.getEmail(),
+        user.getPassword(),
+        user.getPin(),
+        user.getGender(),
+        java.sql.Date.valueOf(user.getBirthday()) // LocalDate to SQL Date
+    );
   }
 }
